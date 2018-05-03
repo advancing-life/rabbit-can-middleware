0cd085aefe0ae9aa03185d4a2c49363671902e3e
aznalo <irimamekun@gmail.com>
2018-05-04 04:46:30 +0900

    Change: Indent

--- config/Router/Router.go
+++ config/Router/Router.go
@@ -1,30 +1,29 @@
 package Router
 
 import (
-    "app/controllers/API"
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
+	"app/controllers/API"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func Init() *echo.Echo {
-    e := echo.New()
+	e := echo.New()
 
-    // Middleware
-    e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-        AllowOrigins: []string{"*"},
-        AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
-    }))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
+	}))
 
+	// Route
+	v1 := e.Group("api/v1")
+	{
+		v1.GET("/", API.Index)
+		v1.GET("/connection", API.Connection)
+	}
 
-    // Route
-    v1 := e.Group("api/v1")
-    {
-        v1.GET("/", API.Index)
-        v1.GET("/connection", API.Connection)
-    }
-
-    return e
+	return e
 }
