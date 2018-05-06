730be2fafd9e8c74e872122d09a5b087eead3560
aznalo <irimamekun@gmail.com>
2018-05-06 14:43:59 +0900

    Install: GoPackage envconfig

--- server.go
+++ server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "app/config/Router"
+	"app/config/Router"
+	"github.com/kelseyhightower/envconfig"
 )
 
 func main() {
-    router := Router.Init()
+	router := Router.Init()
 	router.Logger.Fatal(router.Start(":1234"))
 }
