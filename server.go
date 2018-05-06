8f9dae884cc81caa0371af1ff416263e367b4178
aznalo <irimamekun@gmail.com>
2018-05-06 15:25:46 +0900

    Setting: ENV

--- server.go
+++ server.go
@@ -3,9 +3,29 @@ package main
 import (
 	"app/config/Router"
 	"github.com/kelseyhightower/envconfig"
+	"log"
 )
 
+type Config struct {
+	Server Server
+	Redis  Redis
+}
+
+type Server struct {
+	Host string `default:"http://localhost"`
+	Port string `default:":1234"`
+}
+
+type Redis struct {
+	Host string `default:"redis"`
+	Port string `default:":6379"`
+}
+
 func main() {
+	var config Config
+	envconfig.Process("", &config)
+
+	log.Println(config)
 	router := Router.Init()
-	router.Logger.Fatal(router.Start(":1234"))
+	router.Logger.Fatal(router.Start(config.Server.Port))
 }
